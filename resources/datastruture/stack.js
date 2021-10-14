b1966b35a32cf10cc1dc89ddedb0bd99195b45f6
leedawnn <2g_hee@naver.com>
2021-10-14 15:38:27 +0900

    feat: stack peek, isEmpty

---
+++ resources/datastruture/stack.js
@@ -0,0 +1,46 @@
+class Stack {
+  constructor() {
+    this.items = [];
+    this.count = 0;
+  }
+
+  push(element) {
+    this.items[this.count] = element;
+    console.log(`stack[${this.count}]: ${element} Added`);
+    this.count++;
+    return this.count-1;
+  } // someStack.push('coke')
+
+  pop() {
+    if (this.count === 0) return undefined;
+
+    let toRemove = this.items[this.count - 1];
+    this.count--;
+    console.log(`stack[${this.count}]: ${to.toRemove} removed`);
+  } // someStack.pop()
+
+  peek() {
+    console.log(`stack[peek> ${this.count-1}]: ${this.items[this.count-1]}`);
+    return this.items[this.count-1];
+  }
+
+  isEmpty() {
+    console.log(this.count === 0)
+    return this.count === 0
+  } // return true or false
+
+  size() {}
+  clear() {}
+
+}
+const stack = new Stack();
+
+stack.push('apple');
+stack.push('banana');
+stack.push('coconut');
+
+stack.pop();
+stack.pop();
+
+stack.isEmpty();
+stack.peek();
